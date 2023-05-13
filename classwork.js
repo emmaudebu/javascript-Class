0b362d61eaa7685b4e8db98ffc1f63a8b0d3bbf0
emmaudebu <29877925+emmaudebu@users.noreply.github.com>
2023-05-13 11:03:24 +0100

    Create classwork.js

---
+++ classwork.js
@@ -0,0 +1,17 @@
+// let isrealOrange = true
+
+
+// isrealOrange ? console.log(" isreal is wearing Orange") : console.log("isreal is not wearing ORANGE")
+
+
+
+let now = new Date();
+
+console.log(now)
+console.log(now.getFullYear())
+console.log(now.getMonth()+1)
+console.log(now.getDay())
+console.log(now.getHours())
+console.log(now.getMinutes())
+console.log(now.getSeconds())
+console.log(now.getTime())
