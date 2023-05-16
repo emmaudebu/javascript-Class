06e20dfcc1d419999907580a9cd6c93add6f2d2e
emmaudebu <29877925+emmaudebu@users.noreply.github.com>
2023-05-16 14:20:23 +0100

    updated

---
+++ Loop.js
@@ -0,0 +1,20 @@
+let NameOfStudents = [];
+NameOfStudents.push("Nduka", "Justina", "Evan", "Kelechi", "Dolap", "folarin", "Isreal", "ifeanyi");
+
+
+console.log(NameOfStudents)
+
+NameOfStudents.push("Gabrial")
+console.log(NameOfStudents)
+
+NameOfStudents.pop("Gabrial")
+
+console.log(NameOfStudents)
+
+NameOfStudents.shift("Nduka")
+console.log(NameOfStudents)
+
+NameOfStudents.unshift("JULIUS")
+console.log(NameOfStudents)
+
+
