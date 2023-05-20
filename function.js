81bfc345c267053d501c4b7eec688371dc13efd2
emmaudebu <29877925+emmaudebu@users.noreply.github.com>
2023-05-20 10:28:19 +0100

    Create function.js

---
+++ function.js
@@ -0,0 +1,12 @@
+function GetName() {
+    let Name = "ifeanyi"
+    let lastName = "Chukwu"
+    let Age = 27;
+    let PhoneNumber = "+2348137935760"
+    
+    console.log(`My Name is ${Name} ${lastName} , i am ${Age} Years And My Number is ${PhoneNumber}`)
+    
+}
+
+
+GetName()
